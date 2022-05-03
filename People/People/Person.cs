ce1cdf6997e29f3828629d5d4d105bd04a0f5c62
AlexMorales9696 <Alex789608>
2022-05-03 17:37:29 +0200

    Aggiunta Classe : Studente

--- People/People/Person.cs
+++ People/People/Person.cs
@@ -1,12 +1,23 @@
 ﻿namespace People
 {
-    public class Person
+    public class Person 
     {
         public int IdPerson { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public DateTime Birthday { get; set; }
         public string Address { get; set; }
+
+        public Person(int idperson,string name,string surname,DateTime birthday,string address)
+        {
+            IdPerson = idperson;
+            Name = name;
+            Surname = surname;
+            Birthday = birthday;
+            Address = address;
+
+
+        }
 
 
     }
---
+++ People/People/Student.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace People
+{
+    internal class Student : Person 
+    {
+        public int IdStudent;
+        public int Matricola;
+        public DateTime DataIscrzione;
+
+        public Student(int idperson, string name, string surname, DateTime birthday, string address,int idstudente,int matrcola,DateTime dataiscrizone) : base(idperson, name, surname,birthday,address)
+        {
+            IdStudent = idstudente;
+            Matricola = matrcola;
+            DataIscrzione = dataiscrizone;
+
+        }
+    }
+}
