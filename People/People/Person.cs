f9d488d820c278f9128e104e0160ed9da201c5aa
AlexMorales9696 <Alex789608>
2022-05-03 17:22:24 +0200

    Creata Classe Person

---
+++ People/People/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace People
+{
+    public class Person
+    {
+        public int IdPerson { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Address { get; set; }
+
+
+    }
+}
