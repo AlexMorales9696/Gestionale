f7713ffe173c0d4300738fd2503a238d61222497
AlexMorales9696 <Alex789608>
2022-05-03 17:44:50 +0200

    Aggiunta Classe: Teacher

---
+++ People/People/Teacher.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace People
+{
+    internal class Teacher : Person
+    {
+        public int IdTeacher;
+        public int MatricolaTeacher;
+        public DateTime DataAssunzione;
+
+        public Teacher(int idperson, string name, string surname, DateTime birthday, string address,int idteacher,int matricolateacher, DateTime dataassunzione) : base( idperson,  name, surname, birthday, address)
+        {
+            IdTeacher = idteacher;
+            MatricolaTeacher = matricolateacher;
+            DataAssunzione = dataassunzione;
+        }
+    }
+}
